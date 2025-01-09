3aa0f472c45fa3da31080d41d32eff5260658953
enzocosson <enzolemercier@gmail.com>
2025-01-09 12:49:27 +0100

    fix : conflicts branch

--- src/App.jsx
+++ src/App.jsx
@@ -17,11 +17,41 @@ import {
   SMAA,
 } from "@react-three/postprocessing";
 import Disque from "./Disque";
+import tchikita from "./assets/JUL_tchikita.mp3";
+import ovni from "./assets/ovni.mp3";
+import moto from './assets/moto.mp3'
+import vient from './assets/vientdela.mp3'
 
 const App = () => {
   const [textureUrl, setTextureUrl] = useState(null);
   const [resetKey, setResetKey] = useState(0);
+  const [isSoundOn, setIsSoundOn] = useState(false);
+  const [lastPlayed, setLastPlayed] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  
+  const sounds = {
+    tchikita: new Audio(tchikita),
+    ovni: new Audio(ovni),
+    moto: new Audio(moto),
+    vient: new Audio(vient),
+  };
+
+  Object.values(sounds).forEach((sound) => {
+    sound.volume = 0.75;
+  });
+
+  const playRandomSound = () => {
+    const soundKeys = Object.keys(sounds);
+    let newSoundKey;
+
+    do {
+      newSoundKey = soundKeys[Math.floor(Math.random() * soundKeys.length)];
+    } while (newSoundKey === lastPlayed);
+
+    setLastPlayed(newSoundKey);
+    sounds[newSoundKey].currentTime = 0;
+    sounds[newSoundKey].play();
+  };
 
   gsap.registerPlugin(ScrollTrigger);
 
@@ -37,6 +67,8 @@ const App = () => {
   }, [resetKey]);
 
   const handleReset = () => {
+    setIsSoundOn(!isSoundOn)
+    playRandomSound();
     setIsLoading(true);
     setTextureUrl(null);
     setResetKey((prevKey) => prevKey + 1);
@@ -45,6 +77,7 @@ const App = () => {
       setIsLoading(false);
     }, 2000);
   };
+  
   const titre_jul = useRef(null);
   const titre_cover = useRef(null);
   const titre_txt = useRef(null);
